e3b9a86dbaf0a72805977d6ba51ccfded861d6c2
jh022912 <thejakesterh@gmail.com>
2026-04-09 23:09:10 -0600

    Simplify CORS to default config

--- Backend/server.js
+++ Backend/server.js
@@ -13,10 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  credentials: false
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
